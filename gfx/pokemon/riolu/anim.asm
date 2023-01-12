889bd1312fb90c3f5a254c32dd9e664c01f34f9b
Zeta_Null <z0zetanull@gmail.com>
2023-01-12 01:54:31 -0500

    Add all of newbox pokecrystal16 at once, with added riolu

---
+++ gfx/pokemon/riolu/anim.asm
@@ -0,0 +1,12 @@
+	frame 1, 08
+	frame 0, 08
+	frame 1, 32
+	frame 0, 04
+	frame 2, 04
+	frame 0, 04
+	frame 2, 04
+	frame 3, 08
+	frame 0, 04
+	frame 3, 08
+	frame 0, 04
+	endanim
---
+++ gfx/pokemon/riolu/anim_idle.asm
@@ -0,0 +1,6 @@
+	frame 3, 08
+	frame 2, 04
+	frame 3, 04
+	frame 2, 04
+	frame 3, 08
+	endanim
